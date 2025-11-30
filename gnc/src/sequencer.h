0982cec76682b64ad8c0cf4059e7d1840719f04f
James Li <68095432+jamm-es@users.noreply.github.com>
2025-11-30 01:40:41 +0000

    support sine waves

--- gnc/src/sequencer.h
+++ gnc/src/sequencer.h
@@ -5,6 +5,8 @@
 
 int sequencer_prepare(int gap, std::vector<float> bps, bool motor_only);
 
+int sequencer_prepare_sine(int total_time, float offset, float amplitude, float period, float phase);
+
 int sequencer_start_trace();
 
 void sequencer_set_data_recipient(int sock);
