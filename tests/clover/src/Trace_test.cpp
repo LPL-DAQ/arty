4603ab443e34b3b11804ff3115e958dd629c5b59
James Li <68095432+jamm-es@users.noreply.github.com>
2026-01-28 13:15:43 -0800

    devcontainer json

--- tests/clover/src/Trace_test.cpp
+++ tests/clover/src/Trace_test.cpp
@@ -4,7 +4,7 @@
 ZTEST(Trace_test, test_get_value)
 {
     Trace trace;
-    zassert_trufalse(trace.sample(0).has_value());
+    zassert_false(trace.sample(0).has_value());
 }
 
 ZTEST_SUITE(Trace_test, NULL, NULL, NULL, NULL, NULL);
