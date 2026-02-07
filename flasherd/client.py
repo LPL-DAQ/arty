5e93f66260f80968d3628efc4bbfa5aa78a042e1
James Li <68095432+jamm-es@users.noreply.github.com>
2026-02-06 23:06:50 -0800

    Fix non utf8 flasherd error, improve log output (#105)

--- flasherd/client.py
+++ flasherd/client.py
@@ -1,6 +1,8 @@
 import flasherd_pb2
 import flasherd_pb2_grpc
 import grpc
+import sys
+import os
 
 def launch_flasherd_command(logger, command_req: flasherd_pb2.RunCommandRequest, binaries: dict[str, str]):
     """
@@ -16,28 +18,30 @@ def launch_flasherd_command(logger, command_req: flasherd_pb2.RunCommandRequest,
             with open(file_path, 'rb') as f:
                 for block in iter(lambda: f.read(4096), b''):
                     reqs.append(flasherd_pb2.RunCommandRequest(binary_name=name, binary_chunk=block))
-
         reqs.append(command_req)
 
-        logger.debug(f'Windows command: {command_req.command_windows}')
-        logger.debug(f'MacOS command: {command_req.command_macos}')
-        logger.debug(f'Linux command: {command_req.command_linux}')
-        logger.info(f'Launching command with args: {[(arg.regular if arg.HasField("regular") else f"{arg.binary} (binary)") for arg in command_req.args]}')
-        logger.debug(f'Binaries: {binaries}')
-
+        command_name = os.path.basename(command_req.command_linux)  # Usually this one is the most minimally formatted.
+        logger.info(f'Launching command `{command_name}` with args: {[(arg.regular if arg.HasField("regular") else f"{arg.binary} (binary)") for arg in command_req.args]}')
+        if len(binaries) != 0:
+            logger.info(f'Binaries: {binaries}')
+            
         try:
             for resp in stub.RunCommand((req for req in reqs)):
                 if resp.HasField('stdout'):
-                    print(resp.stdout.decode(), end='')
+                    sys.stdout.buffer.write(resp.stdout)
                 if resp.HasField('stderr'):
-                    print(resp.stderr.decode(), file=sys.stderr, end='')
+                    sys.stdout.buffer.write(resp.stderr)
                 if resp.HasField('exit_code'):
                     logger.info(f'Process terminated with code {resp.exit_code}')
                     break
         except grpc.RpcError as e:
-            logger.error('Failed to run command via flasherd.')
-            print(e)
             if e.code() == grpc.StatusCode.UNAVAILABLE:
                 logger.error('Failed to connect to flasherd, it might not be running.')
+                
+                # Bail out without printing a massive stacktrace; this keeps the advice that "it might not be running" more apparent to end user.
+                sys.exit(1)
+            else:
+                logger.error('Failed to run command via flasherd: {e}')
+                raise e
 
 
