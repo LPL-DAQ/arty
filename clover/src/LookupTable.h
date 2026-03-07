54f3a88d3cb0214e3f5a059d7e85c7f24e86b74f
James Li <68095432+jamm-es@users.noreply.github.com>
2026-03-07 04:04:58 -0800

    go forth

--- clover/src/LookupTable.h
+++ clover/src/LookupTable.h
@@ -1,65 +1,67 @@
 #ifndef APP_LOOKUP_TABLE_H
 #define APP_LOOKUP_TABLE_H
 
-// template <int len> consteval void validate_axis_bps(std::array<float, len> bps)
-// {
-//     for (int i = 1; i < len; ++i) {
-//         static_assert(bps[i - 1] < bps[i]);
-//     }
-// }
+template <int len> consteval void validate_axis_bps(std::array<float, len> bps)
+{
+    for (int i = 1; i < len; ++i) {
+        static_assert(bps[i - 1] < bps[i]);
+    }
+}
 
-// float tween(float in, float low_in, float high_in, float low_out, float high_out)
-// {
-//     return low_out + (in - low_in) / (high_in - low_in) * (high_out - low_out);
-// }
+float tween(float in, float low_in, float high_in, float low_out, float high_out)
+{
+    return low_out + (in - low_in) / (high_in - low_in) * (high_out - low_out);
+}
 
-// template <int x_len, std::array<float, x_len> x_bps, int y_len, std::array<float, y_len> y_bps, std::array<std::array<y_len>, x_len> bps> class LookupTable {
-//     float interp(float x, float y);
-// }
+template <int x_len, std::array<float, x_len> x_bps, int y_len, std::array<float, y_len> y_bps, std::array<std::array<y_len>, x_len> bps> class LookupTable {
+    float interp(float x, float y);
+}
 
-// template <int x_len, std::array<float, x_len> x_bps, int y_len, std::array<float, y_len> y_bps, std::array<std::array<y_len>, x_len> bps>
-// float LookupTable<x_len, x_bps, y_len, y_bps, bps>::interp(float x, float y)
-// {
-//     static_assert(x_len >= 2);
-//     static_assert(y_len >= 2);
-//     validate_axis_bps<x_len>(x_bps);
-//     validate_axis_bps<y_len>(y_bps);
+template <int x_len, std::array<float, x_len> x_bps, int y_len, std::array<float, y_len> y_bps, std::array<std::array<y_len>, x_len> bps>
+float LookupTable<x_len, x_bps, y_len, y_bps, bps>::interp(float x, float y)
+{
+    static_assert(x_len >= 2);
+    static_assert(y_len >= 2);
+    validate_axis_bps<x_len>(x_bps);
+    validate_axis_bps<y_len>(y_bps);
 
-//     // Find x breakpoint indices
-//     float x_bp_lo = x_bps[x_len-2];
-//     float x_bp_hi = x_bps[x_len-1];
-//     for (int i = 1; i < x_len; ++i) {
-//         if (x < x_bps[i]) {
-//             x_idx_lo = i - 1;
-//             x_idx_hi = i;
-//             break;
-//         }
-//     }
+    // Find x breakpoint indices
+    float x_bp_lo = x_bps[x_len-2];
+    float x_bp_hi = x_bps[x_len-1];
+    for (int i = 1; i < x_len; ++i) {
+        if (x < x_bps[i]) {
+            x_idx_lo = i - 1;
+            x_idx_hi = i;
+            break;
+        }
+    }
 
-//     // Find y breakpoints indices
-//     float y_idx_lo = y_len - 2;
-//     float y_idx_hi = y_len - 1;
-//     for (int i = 1; i < y_len; ++i) {
-//         if (y < y_bps[i]) {
-//             y_idx_lo = i - 1;
-//             y_idx_hi = i;
-//             break;
-//         }
-//     }
+    // Find y breakpoints indices
+    float y_idx_lo = y_len - 2;
+    float y_idx_hi = y_len - 1;
+    for (int i = 1; i < y_len; ++i) {
+        if (y < y_bps[i]) {
+            y_idx_lo = i - 1;
+            y_idx_hi = i;
+            break;
+        }
+    }
 
-//     // Keep x low, tween y
-//     float xlo_ylo = bps[x_idx_lo][y_idx_lo];
-//     float xlo_yhi = bps[x_idx_lo][y_idx_hi];
-//     float xlo_ytween = tween(y, y_bps[y_idx_lo], y_bps[y_idx_hi], bps[x_idx_lo][y_idx_lo], bps[x_idx_lo][y_idx_hi]);
+    // Keep x low, tween y
+    float xlo_ylo = bps[x_idx_lo][y_idx_lo];
+    float xlo_yhi = bps[x_idx_lo][y_idx_hi];
+    float xlo_ytween = tween(y, y_bps[y_idx_lo], y_bps[y_idx_hi], bps[x_idx_lo][y_idx_lo], bps[x_idx_lo][y_idx_hi]);
 
-//     // Keep x high, tween y
-//     float xhi_ylo = bps[x_idx_lo][y_idx_lo];
-//     float xhi_yhi = bps[x_idx_lo][y_idx_lo];
-//     float xhi_ytween = tween(y, y_bps[y_idx_lo], y_bps[y_idx_hi], bps[x_idx_lo][y_idx_lo], bps[x_idx_lo][y_idx_hi]);
+    // Keep x high, tween y
+    float xhi_ylo = bps[x_idx_lo][y_idx_lo];
+    float xhi_yhi = bps[x_idx_lo][y_idx_lo];
+    float xhi_ytween = tween(y, y_bps[y_idx_lo], y_bps[y_idx_hi], bps[x_idx_lo][y_idx_lo], bps[x_idx_lo][y_idx_hi]);
 
-//     // Tween x with y-tweened breakpoints.
-//     return tween(x, x_bps[x_idx_lo], x_bps[x_idx_hi], xlo_ytween, x_hi_ytween);
-// }
+    // Tween x with y-tweened breakpoints.
+    return tween(x, x_bps[x_idx_lo], x_bps[x_idx_hi], xlo_ytween, x_hi_ytween);
+}
+
+typedef 
 
 // 2D Bilinear Interpolation Function Declaration
 float interp2D(const float* x_axis, int x_len, const float* y_axis, int y_len, const float* data_grid, float x_val, float y_val);
