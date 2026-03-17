d89e3fe55c544e2ba7c2e8e979840c89fc4a7556
Hungyu Lin <hungyul@usc.edu>
2026-03-17 00:48:28 -0700

    Enable SPI1 and add basic SPI test (not verified on hardware)

--- bees/src/main.cpp
+++ bees/src/main.cpp
@@ -1,45 +1,50 @@
 #include <zephyr/kernel.h>
-#include <zephyr/drivers/gpio.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/spi.h>
 
-/* ---- Denny ---- */
-#include <stdint.h>
-
-__attribute__((section(".ext_rodata")))
-const uint8_t test_table[1024] = {1};
-/* ---- Denny ---- */
-
-static const device* rcc_dev = DEVICE_DT_GET(DT_NODELABEL(rcc));
-static gpio_dt_spec dir_gpio = GPIO_DT_SPEC_GET(DT_PATH(zephyr_user), led_test_gpios);
+static const struct device *spi_dev = DEVICE_DT_GET(DT_NODELABEL(spi1));
 
 int main(void)
 {
-    int my_var = rcc_dev->state->init_res;
-    if(my_var == 111) {
-        k_sleep(K_MSEC(1000));
-        return 1;
+    if (!device_is_ready(spi_dev)) {
+        return 0;
     }
 
-    int ret = device_is_ready(rcc_dev);
-    if(!ret) {
-        k_sleep(K_MSEC(1000));
-        return 1;
-    }
+    uint8_t tx_buf[2] = {0xAA, 0x55};
+    uint8_t rx_buf[2] = {0};
 
-    ret = gpio_is_ready_dt(&dir_gpio);
-    if (!ret) {
-        k_sleep(K_MSEC(1000));
-		return 0;
-	}
+    struct spi_buf tx_bufs[] = {
+        {
+            .buf = tx_buf,
+            .len = sizeof(tx_buf)
+        }
+    };
 
-	ret = gpio_pin_configure_dt(&dir_gpio, GPIO_OUTPUT_ACTIVE);
-	if (ret < 0) {
-        k_sleep(K_MSEC(1000));
-		return 0;
-	}
+    struct spi_buf rx_bufs[] = {
+        {
+            .buf = rx_buf,
+            .len = sizeof(rx_buf)
+        }
+    };
 
-    while(true) {
-        gpio_pin_toggle_dt(&dir_gpio);
-        k_sleep(K_MSEC(500));
-        // gpio_pin_set_dt(&dir_gpio, 1);
+    struct spi_buf_set tx = {
+        .buffers = tx_bufs,
+        .count = 1
+    };
+
+    struct spi_buf_set rx = {
+        .buffers = rx_bufs,
+        .count = 1
+    };
+
+    struct spi_config spi_cfg = {
+        .frequency = 1000000,
+        .operation = SPI_WORD_SET(8) | SPI_TRANSFER_MSB,
+        .slave = 0,
+    };
+
+    while (1) {
+        spi_transceive(spi_dev, &spi_cfg, &tx, &rx);
+        k_sleep(K_MSEC(1000));
     }
 }
